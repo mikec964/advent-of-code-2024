168d6d77c33541f82b11412a217e249b313cb1eb
Mike Combs <mcombs964@gmail.com>
2024-12-12 01:59:20 -0500

    cleaned calcs, return result and solution

--- day-2.py
+++ day-2.py
@@ -12,37 +12,8 @@ report_text:str = """\
     """
 
 
-def calc_is_safe(report:list[int]) -> bool:
-    found_problem: bool = False  # Any violation makes this True
-    bad_levels:int = 0
-    for idx, level in enumerate(report):
-        if idx == 0:
-            previous_level: int = level
-            continue
-        if idx == 1:
-            is_increasing:bool = (level >= previous_level)
-        if is_increasing and level <= previous_level:
-            # print("Stopped increasing")
-            found_problem = True
-        if (not is_increasing) and level >= previous_level:
-            # print("Stopped decreasing")
-            found_problem = True
-        # print(abs(level - previous_level), end=None)
-        if abs(level - previous_level) > 3 or (abs(level - previous_level) < 1):
-            # print("Change is <1 or >3")
-            found_problem = True
-        if found_problem:
-            return False
-        else:
-            previous_level = level
-    return True
-
-
 def load_ints_from_rows(rows: list[str]) -> list[list[int]]:
-    """Parse text lines with numbers, return lists of ints.
-    
-    Numbers in columns are separated by white space."""
-
+    """Parse strings with white space separated ints, return lists of ints."""
     reports:list[list[int]] = []
     for line in rows:
         items:list[int] = [int(item) 
@@ -51,23 +22,48 @@ def load_ints_from_rows(rows: list[str]) -> list[list[int]]:
     return reports
 
 
-def calc_safe_with_dampener(report:list[int], dampener:int = 1) -> bool:
-    # If a report fails, loop through removving a level to see if it passes
+def calc_is_safe(report:list[int]) -> bool:
+    """Return true if numbers in list follow rules.
+    
+    All numbers must be increasing or decreasing.
+    """
+    is_increasing: bool = False
+    previous_level: int = report[0]
+    for idx, level in enumerate(report):
+        if idx == 0:
+            continue
+        if level == previous_level or abs(level - previous_level) > 3:
+            return False
+        if idx == 1:
+            is_increasing = (level > previous_level)
+        if is_increasing and level < previous_level:
+            return False
+        if (not is_increasing) and level > previous_level:
+            return False
+        previous_level = level
+    return True
+
+
+def calc_safe_with_dampener(report:list[int], dampener:int = 1) -> tuple[bool, list[int]]:
+    # If a report fails, loop through removing a level to see if it passes
     is_safe: bool = calc_is_safe(report)
     if is_safe:
-        return True
+        return (True, report)
+    # We start the loop with is_safe == False
     for skip in range(0, len(report)):
-        short_report: list[int] = report[:skip] + report[skip+1:]
-        pp(short_report)
-    return False
+        short_report: list[int] = report[:skip] + report[skip+dampener:]
+        # pp(short_report)
+        if calc_is_safe(short_report):
+            return (True, short_report)
+    return (False, [])
 
 
-def calc_safe_reports(reports:list[list[int]]) -> int:
+def calc_safe_report_count(reports:list[list[int]]) -> int:
     safe_count:int = 0
     for report in reports:
-        is_safe:bool = calc_safe_with_dampener(report, 1)
-        print(report, is_safe)
+        is_safe, solution = calc_safe_with_dampener(report)
         safe_count += 1 if is_safe else 0
+        print(report, solution, is_safe, safe_count)
     return safe_count
 
 
@@ -75,16 +71,11 @@ print("======= SAMPLE DATA ========")
 report_lines:list[str] = dedent(report_text).splitlines()
 reports: list[list[int]] = load_ints_from_rows(report_lines)
 # pp(reports)
-print(f"{calc_safe_reports(reports)} reports are safe.")
-# print(calc_safe_with_dampener(reports[1]))
-# print(calc_safe_with_dampener(reports[2]))
-# print(calc_safe_with_dampener(reports[3]))
-# print(calc_safe_with_dampener(reports[4]))
-# print(calc_safe_with_dampener(reports[5]))
+print(f"{calc_safe_report_count(reports)} reports are safe.")
 
-# print("======= REAL DEAL ========")
-# with open("day-2-input.txt", "r") as f:
-#     report_lines = f.readlines()
-# reports: list[list[int]] = load_ints_from_rows(report_lines)
-# # pp(reports)
-# print(f"{calc_safe_reports(reports)} reports are safe.")
+print("======= REAL DEAL ========")
+with open("day-2-input.txt", "r") as f:
+    report_lines = f.readlines()
+reports: list[list[int]] = load_ints_from_rows(report_lines)
+# pp(reports)
+print(f"{calc_safe_report_count(reports)} reports are safe.")
